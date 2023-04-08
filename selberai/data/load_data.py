b6f115622e12bc35909baa398f4ee869d8b884d0
ArsamAryandoust <arsama@ethz.ch>
2023-04-08 23:39:17 +0200

    extended load data

--- selberai/data/load_data.py
+++ selberai/data/load_data.py
@@ -5,6 +5,8 @@ import numpy as np
 
 import selberai.data.download_data as download_data
 
+
+
 def load(name: str, sample_only=False, path_to_data=None, token=None) -> (
   np.array, np.array, np.array):
   """
@@ -40,20 +42,35 @@ def load(name: str, sample_only=False, path_to_data=None, token=None) -> (
   if not data_avail:
     download_data.download(name, path_to_data, token)
     
+  ###
+  # Load training, validation and testing ###
+  ###
+  
   # set paths and read the directories
   path_to_train = path_to_data + 'training/'
   path_to_val = path_to_data + 'validation/'
   path_to_test = path_to_data + 'testing/'
+  
+  # read directory content
   train_cont = os.listdir(path_to_train)
   val_cont = os.listdir(path_to_val)
   test_cont = os.listdir(path_to_test)
+  
+  # reduce directory content to single file only if sample_only==True
+  if sample_only:
+    train_cont = train_cont[:1]
+    val_cont = val_cont[:1]
+    test_cont = test_cont[:1]
   
   # set empty dataframes
   train = pd.DataFrame()
   val = pd.DataFrame()
   test = pd.DataFrame()
   
-  # iterate and concatenate
+  # set additional default to None. Replace for respective datasets
+  add = None
+  
+  # iterate over train, val, test data files and concatenate
   print("Loading training data.")
   pbar = tqdm(total=len(train_cont))
   for f_name in train_cont:
@@ -69,14 +86,30 @@ def load(name: str, sample_only=False, path_to_data=None, token=None) -> (
   for f_name in test_cont:
     test = pd.concat((test, pd.read_csv(path_to_test+f_name)))
     pbar.update(1)
-    
-  # convert to numpy arrays
-  train = train.to_numpy()
-  val = val.to_numpy()
-  test = test.to_numpy()
+  
+ 
+  ###
+  # Convert to unified data representation and potentially load additional ###
+  ###
+  
+  if name == 'BuildingElectricity':
+    train = convert_be(config[name], train)
+    val = conver_be(config[name], val)
+    test = convert_be(config[name], test)
+    add = load_add_be()
+  elif name == 'WindFarm':
+    print('To Do: Needs to be implemented!')
+
   
   # set and return value
-  return_value = (train, val, test)
+  return_value = (train, val, test, add)
   return return_value
   
+
+def convert_be(config_be: dict, dataset: pd.DataFrame) -> np.array:
+  """
+  """
   
+  dataset = dataset.to_numpy()
+  
+  return dataset
